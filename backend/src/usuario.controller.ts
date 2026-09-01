72c5ba66a14e5f8ad3cbafc72a27312eae996745
Natan.M <natan.mnascimento55@gmail.com>
2026-09-01 14:00:48 -0300

    Add user registration and listing routes with guards
    rota das permissões e login

--- backend/src/usuario.controller.ts
+++ backend/src/usuario.controller.ts
@@ -1,5 +1,7 @@
 /*--- Cria rota protegida que pode ser acessada com token JTW válido ---*/
-
+import { Roles } from './auth/decorators/roles.decorator';
+import { RolesGuard } from './auth/guards/roles.guard';
+import { Role } from './enums/role.enum';
 import { Controller, Get, UseGuards } from '@nestjs/common'; //useguards aplica uma definição de segurança antes da execução
 import { JwtAuthGuard } from './auth/jwt-auth.guard'; //guard q valida se o usuário enviou um JTW valido na header, se não, ele bloqueia
 
@@ -9,5 +11,17 @@ export class UsuarioController {
   @Get('perfil') // cria rota
   getPerfil() {
     return { msg: 'Acesso permitido apenas com token válido!' }; //se o token
+  }
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async cadastrar(@Body() criarUsuarioDto: CriarUsuarioDto) {
+    return this.usuarioService.cadastrar(criarUsuarioDto);
+  }
+}
+@Get('todos')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  async listarTodos() {
+    return this.usuarioService.listarTodos();
   }
 }
